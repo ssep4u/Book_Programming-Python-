6da864997ce8295182bcdd6f3280d9806d55de8f
ssep4u <ssep4u@gmail.com>
2018-05-27 16:37:14 +0900

    modify position

--- WebCrawling1.py
+++ WebCrawling1.py
@@ -1,15 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-# 네이버 웹툰 > 마음의 소리
-# data = urllib.request.urlopen('http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fn')
-# soup = BeautifulSoup(data, 'html5lib')
-# # print(soup)
-# cartoons = soup.findAll('td', attrs = {'class':'title'})
-# for item in cartoons:
-#     title = item.find('a').text
-#     print(title)
-
 # TED Talks > 한국어
 data = urlopen('https://www.ted.com/talks?sort=newest&language=ko')
 soup = BeautifulSoup(data, 'lxml')
@@ -18,3 +9,12 @@ for item in talkVideos:
     title = item.find('a', attrs={'data-ga-context':'talks'}).text
     link = item.find('a')['href']
     print('{} {}'.format(title, link))
+    
+# 네이버 웹툰 > 마음의 소리
+# data = urllib.request.urlopen('http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fn')
+# soup = BeautifulSoup(data, 'html5lib')
+# # print(soup)
+# cartoons = soup.findAll('td', attrs = {'class':'title'})
+# for item in cartoons:
+#     title = item.find('a').text
+#     print(title)
